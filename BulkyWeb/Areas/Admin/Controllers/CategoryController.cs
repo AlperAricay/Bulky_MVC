5da2c88ce18d6ab0b953ee34f1413bc0f016705c
Alper Arıçay <alperaricay@gmail.com>
2023-05-17 07:00:41 +0300

    Completed authorization based on user role

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers;
 [Area("Admin")]
+[Authorize(Roles = SD.RoleAdmin)]
 public class CategoryController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
