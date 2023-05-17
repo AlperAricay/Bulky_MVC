a83f388b7a7d06283a2871b9b975a409423915eb
Alper Arıçay <alperaricay@gmail.com>
2023-05-17 06:50:03 +0300

    Extended identity user, implemented roles & registration

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -2,4 +2,8 @@
 
 public static class SD
 {
+    public const string RoleCustomer = "Customer";
+    public const string RoleCompany = "Company";
+    public const string RoleAdmin = "Admin";
+    public const string RoleEmployee = "Employee";
 }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,18 +1,24 @@
 using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
-using Microsoft.EntityFrameworkCore;
+using Bulky.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options=> 
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -31,6 +37,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{productId?}");
