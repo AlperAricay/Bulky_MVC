49377092b05847cb367507fb3c5bc8ae9309924d
Alper Arıçay <alperaricay@gmail.com>
2023-05-24 18:48:55 +0300

    Order view model & order controller

---
+++ Bulky.Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Bulky.Models.ViewModels;
+
+public class OrderViewModel
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetails { get; set; }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = SD.RoleAdmin)]
+public class OrderController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public OrderController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var objOrderHeaders = _unitOfWork.OrderHeaderRepo.GetAll(includeProperties: "ApplicationUser").ToList();
+        return Json(new {data = objOrderHeaders});
+    }
+    
+    #endregion
+}
