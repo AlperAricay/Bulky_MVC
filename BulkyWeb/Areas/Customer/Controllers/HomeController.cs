d3a6807c4cff69b4c67c52a4509c78da302d3c15
Alper Arıçay <alperaricay@gmail.com>
2023-05-27 19:02:41 +0300

    Moved cart count implementation to view component

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -22,15 +22,6 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        var claimsIdentity = (ClaimsIdentity) User.Identity;
-        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-        if (claim != null)
-        {
-            HttpContext.Session.SetInt32(SD.SessionCart, 
-                _unitOfWork.ShoppingCartRepo.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-        }
-        
         var productList = _unitOfWork.ProductRepo.GetAll(includeProperties: "Category");
         return View(productList);
     }
